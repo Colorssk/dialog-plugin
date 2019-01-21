8ff4bece4f7737333455c7011b94a851c2114119
ming.yan <ming.yan@ly.com>
2019-01-21 14:14:17 +0800

    1.1.1

--- src/components/dialog/index.js
+++ src/components/dialog/index.js
@@ -2,14 +2,12 @@ import Dialogcom from './dialog.vue'
 import dialog from './function'
 import ElementUI from 'element-ui'
 import 'element-ui/lib/theme-chalk/index.css';
-import Dialog from './components/dialog'
 export default (Vue) => {
   Vue.component(Dialogcom.name, Dialogcom)
   if (typeof window !== 'undefined' && window.Vue) {
     window.$dialog = dialog
     Vue.use(ElementUI)
-    Vue.use(Dialog)
     Vue.use(dialog)
   }
-  // Vue.prototype.$dialog = dialog
+  Vue.prototype.$dialog = dialog
 }
