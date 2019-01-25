67ddf27f97ab130845c092bfa68910d0176b781a
ming.yan <ming.yan@ly.com>
2019-01-25 13:54:00 +0800

    next-06

--- src/lib/index.js
+++ src/lib/index.js
@@ -2,7 +2,6 @@ import Dialogcom from './dialog.vue'
 import dialog from './function'
 import ElementUI from 'element-ui'
 import 'element-ui/lib/theme-chalk/index.css'
-
 export default (Vue) => {
   Vue.component(Dialogcom.name, Dialogcom)
   Vue.prototype.$dialog = dialog
