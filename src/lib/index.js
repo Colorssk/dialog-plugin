ff02733cfe81781dd8b81406b77b75dc8da678df
ming.yan <ming.yan@ly.com>
2019-01-25 17:16:00 +0800

    取消window判断

--- src/lib/index.js
+++ src/lib/index.js
@@ -4,9 +4,9 @@ import dialog from './function'
 export default (Vue) => {
   Vue.component(Dialogcom.name, Dialogcom)
   Vue.prototype.$dialog = dialog
-  if (typeof window !== 'undefined' && window.Vue) {
-    window.Vue.use(dialog)
-  }
+  // if (typeof window !== 'undefined' && window.Vue) {
+  //   window.Vue.use(dialog)
+  // }
 }
 // const pubdialog = {
 //   install(Vue, options){
