9bd2aa69e6bb2df37c70735b780c18df66d470af
ming.yan <ming.yan@ly.com>
2019-01-25 13:39:35 +0800

    next

--- src/components/dialog/falseRules.js
+++
@@ -1,6 +0,0 @@
-
-export default {
-  input: { required: false, message: '请按要求输入', trigger: 'blur' },
-  select: { required: false, message: '请按要求选择', trigger: ['blur', 'change'] },
-  radio: { required: false, message: '请按要求输入', trigger: 'blur' }
-}
--- src/components/dialog/rules.js
+++
@@ -1,7 +0,0 @@
-
-const rules = {
-  input: { required: true, message: '请按要求输入', trigger: 'blur' },
-  select: { required: true, message: '请按要求选择', trigger: ['blur', 'change'] },
-  radio: { required: true, message: '请按要求输入', trigger: 'blur' }
-}
-export default rules
